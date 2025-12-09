52c364ded870baabe5af5fe5c6a95af4f7b3c320
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-12-09 15:08:29 +0000

    created sample search feature

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://r2.theaudiodb.com/images/media/artist/thumb/**')]
+  }
 };
 
 export default nextConfig;
